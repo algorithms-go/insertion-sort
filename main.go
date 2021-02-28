78e6503657841fd61c0cf3745350d4535d73c277
steevehook <steevehook@gmail.com>
2021-03-01 00:07:46 +0200

    improve the presentation

--- main.go
+++ main.go
@@ -5,16 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Println(insertionSort([]int{5,2,4,6,1,3}))
+	fmt.Println(insertionSort([]int{5, 2, 4, 6, 1, 3}))
 }
 
+// insertionSort sorts a given array/slice using the INSERTION SORT algorithm
+// 1. We iterate from left to right
+// 2. The left side is always sorted
+// 3. We take each element an insert it into its correct position
 func insertionSort(A []int) []int {
 	for j := 1; j < len(A); j++ {
 		key := A[j]
-		i := j-1
-		for i >	-1 && A[i] > key {
+		i := j - 1
+		for i > -1 && A[i] > key {
 			A[i+1] = A[i]
-			i-=1
+			i -= 1
 		}
 		A[i+1] = key
 	}
